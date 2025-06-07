8ea8eaffb2297376e66787fa5259605f1823ea99
Paulo Pereira Fernandes Oliveira <ppoliveira_lider@timbrasil.com.br>
2025-06-07 18:28:22 -0300

    host - 07/06/2025

--- portf/settings.py
+++ portf/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-=dqcldpoq^#c1g9)pf$sqee(rj5w%@a680u5o^dwb(zi9wx8ug
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
